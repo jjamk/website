e95c68ede4dc78ea446858750424519da5d05035
jjamk <59911373+jjamk@users.noreply.github.com>
2021-03-31 17:08:58 +0900

    2

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+    $("#container").addClass("start");
 	$("nav li").click(function(){
+        $("#container").css("max-width", "100%");
+        var id=$(this).attr("data-rol");
         $("nav li").removeClass("on");
         $(this).addClass("on");
+        $("content").removeClass("prev this next");
+        $("#" + id).addClass("this").prevAll().addClass("prev");
+        $("#"+id).nextAll().addClass("next");
+    });
+    $(".logo_box").click(function(){
+        $("nav li").removeClass("on");
+         $(".content").removeClass("prev this next");
+         $("#container").css("max-width", "1200px");
     });
 });
-
