f1789687cbeea29c49f67504d3f3fba43833994b
jjamk <59911373+jjamk@users.noreply.github.com>
2021-04-26 23:34:42 +0900

    contact us(-ing)

--- js/ui.js
+++ js/ui.js
@@ -40,5 +40,10 @@ $(document).ready(function(){
 			}
 		});
 	});
+	//faq 게시판
+	$(".accordio_box ol li").click(function(){
+		$(".accordio_box ol li").removeClass("on");
+		$(this).addClass("on");
+	});
 });
 
